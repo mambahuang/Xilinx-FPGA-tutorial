6051442cf6859ed0c467bb124475574b9088b85c
CHENSHIBO0000 <bbchen0517@gmail.com>
2025-07-03 15:41:44 +0800

    更新

---
+++ Hardware-Software-Codesign/Lab/Lab-AXI-lite/README.md
@@ -0,0 +1,28 @@
+# FPGA Design Lab
+
+與 Lab1-UART Problem 1 相似，但請改用 Verilog 設計一個 Sorting 電路，打包成 AXI IP 後，使用 block design 的方式來完成此 Lab 。 
+
+## Problem
+設計一個排序電路，由 processor 輸入一串正整數將其排序後傳回。
+
+1. 數字位元數自訂。(最少4bit)
+2. 數列長度自訂且固定。(最少8筆數字)
+3. 排序：大 → 小 、 小 → 大，二選一即可。
+4. 演算法不限。
+
+
+## 參考 block design
+![](png/block_design.png)
+
+
+## 預設結果呈現:(大到小)
+
+```
+77743217 -> 77774321
+
+1fb4a219 -> fba94211
+
+123489af -> fa984321
+```
+
+![](png/answer.png)
--- Hardware-Software-Codesign/Lab/Lab3-CDMA/src/HDL/add.v
+++ Hardware-Software-Codesign/Lab/Lab3-CDMA/src/HDL/add.v
@@ -11,17 +11,17 @@ output	[31:0]	W_data;
 
 wire w_r;
 
-reg		[1:0]	C_state;
-wire	[1:0]	N_state;
+reg		[1:0]	curr_state;
+wire	[1:0]	next_state;
 
 reg 	[31:0]	indata	[1:0];
 
 assign W_data = indata[0][15:0] + indata[1][15:0];
-assign N_state = C_state + 1;
-assign w_r = C_state[0] & C_state[1];
+assign next_state = curr_state + 1;
+assign w_r = curr_state[0] & curr_state[1];
 assign R_req = 1;
 assign W_req = {w_r, w_r, w_r, w_r};
-assign addr = {28'b0,C_state,2'b0};
+assign addr = {28'b0,curr_state,2'b0};
 
 
 
@@ -29,15 +29,15 @@ assign addr = {28'b0,C_state,2'b0};
 always@(posedge clk or negedge rst)begin
 
 	if(!rst)begin
-		C_state <= 0;
+		curr_state <= 0;
 		indata[0] <= 0;
 		indata[1] <= 0;
 		
 	end
 
 	else begin
-		C_state <= N_state;
-		indata[N_state[0]]<=R_data;
+		curr_state <= next_state;
+		indata[next_state[0]]<=R_data;
 	end
 
 
